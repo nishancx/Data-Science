552cf029b497c7ecdd745ca60c1d402059fb6a44
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2022-05-01 08:50:21 +0545

    Cleanup

--- April31/task1_convert_list_to_dict.py
+++ April31/task1_convert_list_to_dict.py
@@ -1,17 +1,18 @@
 """Program to convert list to dict conditionally"""
 
-def convert_list_to_dict(myDict:dict, keysList:list, listValues:list):
-    """Convert list to dict conditionally
+def convert_list_to_dict(my_dict:dict, keys_ist:list, values_list:list):
+    """convert list to dict conditionally
 
     Args:
-        dict_a (_type_): _description_
-        list_keys (_type_): _description_
-        list_values (_type_): _description_
+        my_dict (dict): _description_
+        keys_ist (list): _description_
+        values_list (list): _description_
     """
 
-    for key in keysList:
-        if key not in myDict:
-            myDict[key] = listValues[key - 1]
-    print(myDict)
+    for key in keys_ist:
+        if key not in my_dict and len(values_list) > key:
+            my_dict[key] = values_list[key]
+
+    print(my_dict)
 
 convert_list_to_dict({5: 'e', 6: 'f'}, [1, 2, 3, 4], ['a', 'b', 'c', 'd'])
