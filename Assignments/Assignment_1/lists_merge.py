6b5a6932a1d4fd05e9bf25a633c62376e13b47f0
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2022-05-06 22:20:21 +0545

    Assignment 1: lists merge, check if key already exists

--- Assignments/Assignment_1/lists_merge.py
+++ Assignments/Assignment_1/lists_merge.py
@@ -5,7 +5,7 @@ percentage = [70, 40, 60, 90, 70]
 marks_dict = {}
 
 for index, student  in enumerate(name):
-  if percentage[index] > 50:
+  if percentage[index] > 50 and student not in marks_dict:
     marks_dict[student] = percentage[index]
 
 print(marks_dict)
