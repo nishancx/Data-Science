04ad050c50f99db7d8fee18612cf8567d363ff9c
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2022-05-07 07:13:18 +0545

    Assignment1: Formatting

--- Assignments/Assignment_1/append_lists.py
+++ Assignments/Assignment_1/append_lists.py
@@ -1,5 +1,4 @@
-from typing import List
-
+"""_summary_"""
 list_a = [1, 2, 3, 4, 5, 6]
 list_b = [10, 20, 30, 40, 50, 60]
 
--- Assignments/Assignment_1/lists_merge.py
+++ Assignments/Assignment_1/lists_merge.py
@@ -1,11 +1,10 @@
-from typing import List, Dict
-
-name = ['Gita', 'Ram', 'Shyam', 'Sita', 'Gita']
+"""_summary_"""
+name = ["Gita", "Ram", "Shyam", "Sita", "Gita"]
 percentage = [70, 40, 60, 90, 70]
 marks_dict = {}
 
-for index, student  in enumerate(name):
-  if percentage[index] > 50 and student not in marks_dict:
-    marks_dict[student] = percentage[index]
+for index, student in enumerate(name):
+    if percentage[index] > 50 and student not in marks_dict:
+        marks_dict[student] = percentage[index]
 
 print(marks_dict)
