a88a68328dbcf54baf98478bfef18f44a0c2673d
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2022-05-07 09:44:20 +0545

    Add docstring

--- Classwork/May7/ones_zeros.py
+++ Classwork/May7/ones_zeros.py
@@ -1,6 +1,6 @@
 """
-
-"""
+  Create an array of shape (4,3) with all the elements "7
+ and dtype int"""
 import numpy as np
-sevens = 7 * np.ones((4, 3))
+sevens = 7 * np.ones((4, 3), dtype=int)
 print(sevens)
