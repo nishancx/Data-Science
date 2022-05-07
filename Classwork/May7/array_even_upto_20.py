186b3cbc9c6243d997725b9b57b4ea9a544e2ed1
Nishan Chhetri <52850000+nishancx@users.noreply.github.com>
2022-05-07 09:08:11 +0545

    Numpy array

---
+++ Classwork/May7/array_even_upto_20.py
@@ -0,0 +1,4 @@
+"""numpy array initialization"""
+import numpy as np
+arr = np.arange(2, 21, 2)
+print(arr)
---
+++ Classwork/May7/numpy_array.py
@@ -0,0 +1,4 @@
+"""numpy array initialization"""
+import numpy as np
+arr = np.arange(1, 11, 1)
+print(arr)
